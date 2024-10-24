fe7bcef86a17cc7afb1b2d98473df5616e51057b
GoldenElf58 <91762091+GoldenElf58@users.noreply.github.com>
2024-10-24 18:38:35 -0400

    Tiles

---
+++ lookup_tables.py
@@ -0,0 +1,18 @@
+EDGE_TABLE: dict[int, list[tuple[int, int]]] = {
+    0: [],                             # 0000: No corners inside
+    1: [(3, 0)],                       # 0001: Corner 0 inside
+    2: [(0, 1)],                       # 0010: Corner 1 inside
+    3: [(3, 1)],                       # 0011: Corners 0 and 1 inside
+    4: [(1, 2)],                       # 0100: Corner 2 inside
+    5: [(3, 0), (1, 2)],               # 0101: Corners 0 and 2 inside
+    6: [(0, 1), (1, 2)],               # 0110: Corners 1 and 2 inside
+    7: [(3, 2)],                       # 0111: Corners 0, 1, and 2 inside
+    8: [(2, 3)],                       # 1000: Corner 3 inside
+    9: [(2, 0)],                       # 1001: Corners 0 and 3 inside
+    10: [(0, 1), (2, 3)],              # 1010: Corners 1 and 3 inside
+    11: [(2, 1)],                      # 1011: Corners 0, 1, and 3 inside
+    12: [(1, 2), (2, 3)],              # 1100: Corners 2 and 3 inside
+    13: [(3, 0)],                      # 1101: Corners 0, 2, and 3 inside
+    14: [(3, 1)],                      # 1110: Corners 1, 2, and 3 inside
+    15: [],                            # 1111: All corners inside
+}
--- tile.py
+++ tile.py
@@ -1,7 +1,72 @@
+import pygame
+
+from line import Line
+from lookup_tables import EDGE_TABLE
+from rectangle import Rectangle
 
 
 class Tile:
-    def __init__(self, corners: tuple[int, int, int, int], row: int, column: int) -> None:
-        self.corners: tuple[int, int, int, int] = corners
+    def __init__(self, corners: int, row: int, column: int, screen: pygame.Surface,
+                 x, y, side_length, weight=3, color=(255, 255, 255)) -> None:
+        self.corners: int = corners
         self.row: int = row
         self.column: int = column
+        self.screen: pygame.Surface = screen
+        self.x: int = x
+        self.y: int = y
+        self.side_length: int = side_length
+        self.color: tuple[int, int, int] = color
+        self.weight: int = weight
+        self.rect: Rectangle = Rectangle(self.x, self.y, self.side_length, self.side_length, color=self.color,
+                                         screen=self.screen)
+
+    def draw(self) -> None:
+        self.rect.draw()
+
+        for edges in EDGE_TABLE[self.corners]:
+            x1, y1 = self.get_edge_midpoint(edges[0])
+            x2, y2 = self.get_edge_midpoint(edges[1])
+            line = Line(x1, y1, x2, y2, 1, screen=self.screen)
+            line.draw()
+
+        for corner in range(4):
+            if 2 ** corner & self.corners:
+                pygame.draw.circle(self.screen, self.color, self.get_corner_location(corner), self.weight * 3)
+
+    def get_corner_location(self, corner: int) -> tuple[int, int]:
+        match corner:
+            case 0:
+                x = self.x
+                y = self.y
+            case 1:
+                x = self.x + self.side_length
+                y = self.y
+            case 2:
+                x = self.x + self.side_length
+                y = self.y + self.side_length
+            case 3:
+                x = self.x
+                y = self.y + self.side_length
+            case _:
+                raise ValueError("Invalid corner")
+
+        return x, y
+
+    def get_edge_midpoint(self, edge: int) -> tuple[int, int]:
+        match edge:
+            case 0:
+                x = self.x + self.side_length / 2
+                y = self.y
+            case 1:
+                x = self.x + self.side_length
+                y = self.y + self.side_length / 2
+            case 2:
+                x = self.x + self.side_length / 2
+                y = self.y + self.side_length
+            case 3:
+                x = self.x
+                y = self.y + self.side_length / 2
+            case _:
+                raise ValueError("Invalid edge")
+
+        return x, y
