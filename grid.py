c196ea0cfa74010ee88a774975abd8c9b6ea4692
GoldenElf58 <91762091+GoldenElf58@users.noreply.github.com>
2024-10-25 14:18:45 -0400

    Update grid.py

--- grid.py
+++ grid.py
@@ -6,7 +6,7 @@ from tile import Tile
 
 
 class Grid:
-    def __init__(self, columns: int, rows: int, screen: pygame.Surface, font=pygame.font.SysFont("Arial", 20)) -> None:
+    def __init__(self, columns: int, rows: int, screen: pygame.Surface, *, font) -> None:
         self.columns: int = columns
         self.rows: int = rows
         self.screen: pygame.Surface = screen
@@ -24,9 +24,17 @@ class Grid:
                 tile.draw()
 
     def randomize_corners(self) -> None:
-        for row in self.grid:
-            for tile in row:
-                tile.set_corners(random.randint(0, 15))
+      for i in range(self.length + 1):
+          for j in range(self.width + 1):
+            b = random.randint(0, 1)
+            if i < self.length and j < self.width:
+              self.grid[i][j].corners[0] = b
+            if i > 0 and j < self.width:
+              self.grid[i-1][j].corners[1] = b
+            if i < self.length and j > 0:
+              self.grid[i][j-1].corners[3] = b
+            if i > 0 and j > 0:
+              self.grid[i-1][j-1].corners[2] = b
 
     def create_empty_grid(self) -> list[list[Tile]]:
         grid = []
@@ -39,5 +47,5 @@ class Grid:
     def initialize_grid(self) -> None:
         for i, row in enumerate(self.grid):
             for j, tile in enumerate(row):
-                self.grid[i][j] = Tile(0, i, j, self.screen, i * self.width / self.columns, j * self.length / self.rows,
-                                       self.width / self.columns)
+                self.grid[i][j] = Tile([0,0,0,0], i, j, self.screen, i * self.width / self.columns, j * self.length / self.rows,
+                                       self.width / self.columns, font=self.font)
